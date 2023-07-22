3472c226c6da0f9a89bf27cdef3256b976857540
Siddhiraj  Pantoji <siddhirajpantoji@Siddhirajs-MacBook-Pro.local>
2023-07-22 23:18:20 +0530

    invalid examples

---
+++ src/basic-varaible-datatypes/strict-invalid-example-5.js
@@ -0,0 +1,3 @@
+"use strict";
+let x = 3.14;
+delete x;  // SyntaxError: Delete of an unqualified identifier in strict mode.
---
+++ src/basic-varaible-datatypes/strict-invalid-example-6.js
@@ -0,0 +1,3 @@
+"use strict";
+function x(p1, p2) {};
+delete x;   //  This will cause error .SyntaxError: Delete of an unqualified identifier in strict mode.
